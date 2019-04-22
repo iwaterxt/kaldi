2758e7d1aed8971d6876c946f0425da349ea2820
iwaterxt <iwaterxt@gmail.com>
2019-04-23 07:38:17 +0800

    update

--- src/nnet4/nnet-update-parallel.h
+++ src/nnet4/nnet-update-parallel.h
@@ -21,9 +21,13 @@
 #include "util/table-types.h"
 #include "util/kaldi-semaphore.h"
 #include "util/kaldi-thread.h"
+#include "util/common-utils.h"
 #include "itf/options-itf.h"
 #include "nnet4/nnet-nnet.h"
 #include "nnet4/nnet-trnopts.h"
+#include "nnet4/nnet-example.h"
+#include "nnet/nnet-randomizer.h"
+
 
 namespace kaldi{
 namespace nnet4{
@@ -49,58 +53,58 @@ struct NnetParallelTrainOptions{
 		randomize(true),
 		feature_transform(""),
 		objective_function("xent"),
-		max_frames(360000),
-		length_tolerance(5),
-		num_threads(1),
 		frame_weights(""),
 		utt_weights(""),
-		use_gpu("yes")
+		use_gpu("yes"),
+		max_frames(360000),
+		length_tolerance(5),
+		num_threads(1)
 	{}
 
 	void Register(OptionsItf *opts){
 
 	    bool binary = true;
-	    po.Register("binary", &binary, "Write output in binary mode");
+	    opts->Register("binary", &binary, "Write output in binary mode");
 
 	    bool crossvalidate = false;
-	    po.Register("cross-validate", &crossvalidate,
+	    opts->Register("cross-validate", &crossvalidate,
 	        "Perform cross-validation (don't back-propagate)");
 
 	    bool randomize = true;
-	    po.Register("randomize", &randomize,
+	    opts->Register("randomize", &randomize,
 	        "Perform the frame-level shuffling within the Cache::");
 
 	    std::string feature_transform;
-	    po.Register("feature-transform", &feature_transform,
+	    opts->Register("feature-transform", &feature_transform,
 	        "Feature transform in Nnet format");
 
 	    std::string objective_function = "xent";
-	    po.Register("objective-function", &objective_function,
+	    opts->Register("objective-function", &objective_function,
 	        "Objective function : xent|mse|multitask");
 
 	    int32 max_frames = 360000;
-	    po.Register("max-frames", &max_frames,
+	    opts->Register("max-frames", &max_frames,
 	        "Maximum number of frames an utterance can have (skipped if longer)");
 
 	    int32 length_tolerance = 5;
-	    po.Register("length-tolerance", &length_tolerance,
+	    opts->Register("length-tolerance", &length_tolerance,
 	        "Allowed length mismatch of features/targets/weights "
 	        "(in frames, we truncate to the shortest)");
 
 	    int32 num_threads = 1;
-	    po.Register("num-threads", &num_threads,
-	    			"number of threads to training neural network")
+	    opts->Register("num-threads", &num_threads,
+	    			"number of threads to training neural network");
 
 	    std::string frame_weights;
-	    po.Register("frame-weights", &frame_weights,
+	    opts->Register("frame-weights", &frame_weights,
 	        "Per-frame weights, used to re-scale gradients.");
 
 	    std::string utt_weights;
-	    po.Register("utt-weights", &utt_weights,
+	    opts->Register("utt-weights", &utt_weights,
 	        "Per-utterance weights, used to re-scale frame-weights.");
 
 	    std::string use_gpu="yes";
-	    po.Register("use-gpu", &use_gpu,
+	    opts->Register("use-gpu", &use_gpu,
 	        "yes|no|optional, only has effect if compiled with CUDA");
 	}
 
